2f212f5164c85cb8b073ec16b8cc6314603f5e00
Simon Cruanes <simon.cruanes@ahrefs.com>
2026-08-27 16:51:12 +0000

    test: pin oracle encoding to utf-8

--- test/lite/oracle.ml
+++ test/lite/oracle.ml
@@ -2,4 +2,5 @@ let parse ?depth_limit
     ?(context : [ `Document | `Fragment of string ] = `Document) report html =
   Markup.string html
   |> Markup.parse_html ~report ~context ?depth_limit
+       ~encoding:Markup.Encoding.utf_8
   |> Markup.signals
